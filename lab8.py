42710d39fb59f8b81de889717bedc7d11d7efca4
Rajani <rajani.kami004@gmail.com>
2026-03-22 13:33:54 -0400

    start file

---
+++ lab8.py
@@ -0,0 +1,7 @@
+"""
+The program's name: Geometry Calculator
+Your name (the author): Rajani Baraili
+The purpose of the program: To calculate the area and perimeter/circumference of circles and rectangles by importing functions from separate modules.
+Any info about starter code:none
+Date: March 22, 2026
+"""
